8d459c9fa170d7682bee43c45dce0f0323a82107
yaakovmo804 <ktivatstam@gmail.com>
2026-07-30 00:41:09 +0300

    Add Mishnat HaOtiyot offline service worker

---
+++ mishnat-haotiyot/sw.js
@@ -0,0 +1,38 @@
+const CACHE = 'mishnat-haotiyot-live-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './payload-1.js',
+  './payload-2.js',
+  './payload-3.js',
+  './payload-4.js',
+  './manifest.webmanifest',
+  './icon.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+      const copy = response.clone();
+      caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
