65ad33690d1ccae8770a3a97f6c4e9ead995820d
yaakovmo804 <ktivatstam@gmail.com>
2026-07-31 00:44:45 +0300

    Cache Medidaot modular application

--- measure-stam/sw.js
+++ measure-stam/sw.js
@@ -1,7 +1,7 @@
-const CACHE_NAME = 'medidaot-v2-2026-07-31';
+const CACHE_NAME = 'medidaot-v2-2026-07-31b';
 const APP_SHELL = [
-  './', './index.html', './styles.css', './app.js', './manifest.webmanifest',
-  './icon.svg'
+  './', './index.html', './styles.css', './app-1.js', './app-2.js', './app-3.js', './app-4.js',
+  './manifest.webmanifest', './icon.svg'
 ];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
