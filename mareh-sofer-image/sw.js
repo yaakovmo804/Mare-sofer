09fbf15b9c8382d16fb88d43f6f0cfc14a60b9d2
yaakovmo804 <ktivatstam@gmail.com>
2026-08-03 10:40:31 +0300

    Add offline service worker for standalone app

---
+++ mareh-sofer-image/sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'mareh-sofer-standalone-v1';
+const APP_ASSETS = [
+  './',
+  './index.html',
+  './app.css?v=20260803b',
+  './image-core.js?v=20260803b',
+  './app.js?v=20260803b',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
