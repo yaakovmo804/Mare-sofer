cc90fbb0a8a73f288401ac1e55da0a62ae65fe01
yaakovmo804 <ktivatstam@gmail.com>
2026-08-03 09:56:24 +0300

    Add offline cache for Mareh Sofer image restoration

---
+++ measure-stam/image-enhancer-sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'mareh-sofer-image-restoration-v1';
+const APP_ASSETS = [
+  './image-enhancer.html',
+  './image-enhancer.css?v=20260803a',
+  './image-processing-core.js?v=20260803a',
+  './image-enhancer.js?v=20260803a',
+  './manifest-image-enhancer.webmanifest'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match('./image-enhancer.html')))
+  );
+});
